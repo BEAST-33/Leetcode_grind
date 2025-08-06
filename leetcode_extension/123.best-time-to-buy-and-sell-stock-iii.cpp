8f25e6c528b499a51e1b1dacdf3552e68eb9d825
KARTHIK MANIKANDAN RAVIKUMAR <149307601+BEAST-33@users.noreply.github.com>
2025-08-06 21:49:35 +0530

    Implement dynamic programming solutions for stock trading problems

---
+++ leetcode_extension/123.best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+   
+    vector<vector<vector<int>>> dp;
+    int helper(int i, vector<int> &prices, int buy,int limit)
+    {
+        if (i == prices.size())
+            return 0;
+        if(!limit && buy){
+            return 0;
+        }
+            if(dp[i][buy][limit]!=-1)return dp[i][buy][limit];
+        
+        int nottake = helper(i + 1, prices, buy,limit);
+        int val;
+        if (buy)
+        {
+            int take = helper(i + 1, prices, 0,limit-1)-prices[i];
+            val = max(nottake, take);
+        }
+        else
+        {
+            int take = helper(i + 1, prices, 1,limit)+prices[i];
+            val = max(nottake, take);
+        }
+        return dp[i][buy][limit]=val;
+    }
+    int maxProfit(vector<int> &prices)
+    {   
+        
+        dp = vector<vector<vector<int>>>(prices.size(), vector<vector<int>>(2, vector<int>(3, -1)));
+        return helper(0, prices, 1,2);
+      
+    }
+};
---
+++ leetcode_extension/714.best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+   
+    vector<vector<int>> dp;
+    int k;
+    int helper(int i, vector<int> &prices, int buy)
+    {
+        if (i == prices.size())
+            return 0;
+            if(dp[i][buy]!=-1)return dp[i][buy];
+        int nottake = helper(i + 1, prices, buy);
+        int val;
+        if (buy)
+        {
+            int take = helper(i + 1, prices, 0)-prices[i];
+            val = max(nottake, take);
+        }
+        else
+        {
+            int take = helper(i + 1, prices, 1)+prices[i]-k;
+            val = max(nottake, take);
+        }
+        
+        return dp[i][buy]=val;
+    }
+    int maxProfit(vector<int> &prices,int fee)
+    {   k =fee;
+        dp.resize(prices.size(), vector<int>(2, -1));
+        return helper(0, prices, 1);
+      
+    }
+};
