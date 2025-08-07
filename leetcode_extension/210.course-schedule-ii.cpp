e8d8a7a46bae65e966e6da93025ade92a7c7a9b8
KARTHIK MANIKANDAN RAVIKUMAR <149307601+BEAST-33@users.noreply.github.com>
2025-08-07 18:20:53 +0530

    Implement topological sorting for Course Schedule II problem

--- leetcode_extension/210.course-schedule-ii.cpp
+++ leetcode_extension/210.course-schedule-ii.cpp
@@ -9,7 +9,39 @@
 using namespace std;
 class Solution {
 public:
+    bool cycle;
+    void toposort(int i , vector<vector<int>>& adj,vector<int>& visited,stack <int>&st ){
+        visited[i] = 1;
+        for ( auto v : adj[i]){
+            if (!visited[v]){
+                toposort(v,adj,visited,st);
+            }
+            if ( visited[v] ==1 )cycle =true;
+        }
+        st.push(i);
+        visited [i]=2;
+        return;
+    }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        int n = numCourses;
+        cycle =false;
+        for ( auto& ele : prerequisites){
+            adj[ele[1]].push_back(ele[0]);
+        }
+        vector<int> visited(numCourses,0);
+        vector<int> ans;
+        stack <int> st;
+        for ( int i =0;i<n;i++){
+            if(!visited[i])toposort(i,adj,visited,st);
+        }
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        
+        if (!cycle)return ans;
+        else return {};
         
     }
 };
